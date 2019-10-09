e3d6b41c4292cf9be4bbfaeea907c6fcecd719bb
Paweł Głomski <pavel.glomski@gmail.com>
2019-10-09 14:08:52 +0200

    More warnings flags

--- src/Tests/EntityList.cpp
+++ src/Tests/EntityList.cpp
@@ -8,49 +8,27 @@ TEST(EntityList, allocEntity)
     EntityList list;
     auto       entity = list.allocEntity(PoolIdx(0), SpawnerID(0));
 
-    ASSERT_EQ(entity.listIdx.value, 0);
-    ASSERT_EQ(entity.version.value, 0);
+    ASSERT_EQ(entity.listIdx.value, 0u);
+    ASSERT_EQ(entity.version.value, 0u);
 
     list.freeEntity(entity);
     entity = list.allocEntity(PoolIdx(0), SpawnerID(0));
-    ASSERT_EQ(entity.listIdx.value, 0);
-    ASSERT_EQ(entity.version.value, 1);
+    ASSERT_EQ(entity.listIdx.value, 0u);
+    ASSERT_EQ(entity.version.value, 1u);
 
     entity = list.allocEntity(PoolIdx(1), SpawnerID(1));
-    ASSERT_EQ(entity.listIdx.value, 1);
-    ASSERT_EQ(entity.version.value, 0);
+    ASSERT_EQ(entity.listIdx.value, 1u);
+    ASSERT_EQ(entity.version.value, 0u);
 
     for (int i = 0; i < 1023; ++i)
         list.allocEntity(PoolIdx(0), SpawnerID(0));
     auto entity2 = list.allocEntity(PoolIdx(1), SpawnerID(1));
-    ASSERT_EQ(entity2.listIdx.value, 1 + 1024);
-    ASSERT_EQ(entity2.version.value, 0);
-    ASSERT_EQ(entity.listIdx.value, 1);
-    ASSERT_EQ(entity.version.value, 0);
+    ASSERT_EQ(entity2.listIdx.value, 1u + 1024u);
+    ASSERT_EQ(entity2.version.value, 0u);
+    ASSERT_EQ(entity.listIdx.value, 1u);
+    ASSERT_EQ(entity.version.value, 0u);
 }
 
-TEST(EntityList, allocEntity)
+TEST(EntityList, t)
 {
-    EntityList list;
-    auto       entity = list.allocEntity(PoolIdx(0), SpawnerID(0));
-
-    ASSERT_EQ(entity.listIdx.value, 0);
-    ASSERT_EQ(entity.version.value, 0);
-
-    list.freeEntity(entity);
-    entity = list.allocEntity(PoolIdx(0), SpawnerID(0));
-    ASSERT_EQ(entity.listIdx.value, 0);
-    ASSERT_EQ(entity.version.value, 1);
-
-    entity = list.allocEntity(PoolIdx(1), SpawnerID(1));
-    ASSERT_EQ(entity.listIdx.value, 1);
-    ASSERT_EQ(entity.version.value, 0);
-
-    for (int i = 0; i < 1023; ++i)
-        list.allocEntity(PoolIdx(0), SpawnerID(0));
-    auto entity2 = list.allocEntity(PoolIdx(1), SpawnerID(1));
-    ASSERT_EQ(entity2.listIdx.value, 1 + 1024);
-    ASSERT_EQ(entity2.version.value, 0);
-    ASSERT_EQ(entity.listIdx.value, 1);
-    ASSERT_EQ(entity.version.value, 0);
 }
